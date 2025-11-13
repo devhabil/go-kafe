5aedf108b0c520aa1c9e42e5b144fc1679baf96a
devhabil <mhabilarifin1414@gmail.com>
2025-11-13 11:39:32 +0700

    backend

---
+++ backend/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/devhabil/go-kafe/backend/config"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	config.LoadEnv()
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello World!",
+		})
+	})
+
+	router.Run(":3000")
+}
